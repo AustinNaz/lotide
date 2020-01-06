fe1c1ca3af2139f60945a82072f76db2f0abc905
Austin <Nazanator@hotmail.com>
2020-01-06 22:02:25 +0000

    Changed the strings to use String Literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {    // Test if the inputs are equal in type and data
-    console.log('✔️✔️✔️: ' + actual + ' === ' + expected);
+    console.log(`✔️✔️✔️: ${actual} === ${expected}`);
   } else {    // Else return failed
-    console.log('🔴️🔴️🔴️: ' + actual + ' !== ' + expected);
+    console.log(`🔴️🔴️🔴️: ${actual} !== ${expected}`);
   }
 };
 
