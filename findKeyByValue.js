110d608dcffc3ac0c4afa46390eeeda66ce312f7
Austin <Nazanator@hotmail.com>
2020-01-08 18:08:54 +0000

    Found a key based on its value

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const findKeyByValue = function(genreArr, show) {
+  for (genre in genreArr) {   // Iterate through the objects
+    if (genreArr[genre] === show) {   // Find out if the show is the same as entered
+      return genre;   // Return the genre
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {    // Test if the inputs are equal in type and data
+    console.log(`✔️✔️✔️: ${actual} === ${expected}`);
+  } else {    // Else return failed
+    console.log(`🔴️🔴️🔴️: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), 'comedy');
