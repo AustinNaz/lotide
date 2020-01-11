d53c11a85c8834a58f4313653a7074bee953b080
Austin <Nazanator@hotmail.com>
2020-01-11 02:14:38 +0000

    Fixed nested array in object support

--- eqObjects.js
+++ eqObjects.js
@@ -21,15 +21,17 @@ function eqObjects(obj1, obj2) {
   for (objects of Object.keys(obj1)) {
     // console.log(objects);
     if (typeof obj1[objects] === 'object' && typeof obj2[objects] === 'object' &&  !Array.isArray(obj1[objects]) && !Array.isArray(obj2[objects])) {
+      console.log('is Object');
       return eqObjects(obj1[objects], obj2[objects]);
-      // console.log('is Object');
-    } else if (Array.isArray(obj1[objects] && Array.isArray(obj2[objects]))) {
-      isEqual = eqArrays(Object.keys(object1).sort(), Object.keys(object2).sort());
-      // console.log('is Array');
-    } 
+    } else if (Array.isArray(obj1[objects]) && Array.isArray(obj2[objects])) {
+      console.log('is Array');
+      isEqual = eqArrays(Object.keys(obj1[objects]).sort(), Object.keys(obj2[objects]).sort());
+    }
 
     if (obj1[objects] === obj2[objects]) {
       // console.log('equal');
+      isEqual = true;
+    } else if (eqArrays(obj1[objects], obj2[objects])) {
       isEqual = true;
     } else {
       isEqual = false;
@@ -59,10 +61,10 @@ function eqObjects(obj1, obj2) {
 // const cr = {c: '4', d: ['2', 3]}
 // eqObjects(cx, cr)
 
-eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }) // => true
+// eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }) // => true
 
-eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }) // => false
-eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }) // => false
+// eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }) // => false
+// eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }) // => false
 eqObjects({
   a: 2,
   b: {
@@ -70,16 +72,18 @@ eqObjects({
     c: 3,
     d: {
       g: 6,
+      l: [5, 4, 2],
     }
   },
   e: 5
 }, {
   a: 2,
   b: {
-    c: 3,
     k: 5,
+    c: 3,
     d: {
       g: 6,
+      l: [5, 4, 2],
     }
   },
   e: 5
