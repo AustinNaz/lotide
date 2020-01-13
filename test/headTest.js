1d8b2586f1f0c809938b2bcc4043f0af60718f3a
Austin <Nazanator@hotmail.com>
2020-01-13 17:43:46 +0000

    Added mocha and chai, refactored head with M&C

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,20 @@
-const assertEqual = require('../assertEqual')
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([]), 5);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([5,6,7]), 8);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it('return undefined when empty array is added', () => {
+    assert.notExists(head([]), 5);
+  })
+
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns Hello from array with words", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+});
